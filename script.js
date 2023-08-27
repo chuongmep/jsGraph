16e5948514fe16abf1a4379f00ee55fbce7d657c
chuongmep <31106432+chuongmep@users.noreply.github.com>
2023-08-27 17:48:05 +0800

    multi graph box

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Room data for the graphs
 const roomData = [
     { room: 'Living Room', adjacentRooms: ['Kitchen', 'Bedroom', 'Bathroom'] },
     { room: 'Kitchen', adjacentRooms: ['Living Room', 'Dining Room'] },
@@ -5,16 +6,6 @@ const roomData = [
     { room: 'Dining Room', adjacentRooms: ['Kitchen'] },
     { room: 'Bathroom', adjacentRooms: ['Living Room'] }
 ];
-
-const links = [];
-roomData.forEach(source => {
-    source.adjacentRooms.forEach(target => {
-        links.push({ source: source.room, target });
-    });
-});
-
-const nodes = Array.from(new Set(roomData.map(d => d.room)))
-    .map(room => ({ room, color: getRandomColor() }));
 
 function getRandomColor() {
     const letters = '0123456789ABCDEF';
@@ -25,84 +16,119 @@ function getRandomColor() {
     return color;
 }
 
-const svg = d3.select('#graph');
+function generateLinks(data) {
+    const links = [];
+    data.forEach(source => {
+        source.adjacentRooms.forEach(target => {
+            links.push({ source: source.room, target });
+        });
+    });
+    return links;
+}
 
-const width = window.innerWidth;
-const height = window.innerHeight;
+function updateGraph(graphContainer, graphData) {
+    const svg = graphContainer.append('svg')
+        .attr('width', graphContainer.node().clientWidth)
+        .attr('height', graphContainer.node().clientHeight);
 
-svg.attr('width', width)
-    .attr('height', height);
+    const simulation = d3.forceSimulation()
+        .force('link', d3.forceLink().id(d => d.room))
+        .force('charge', d3.forceManyBody().strength(-200))
+        .force('center', d3.forceCenter(svg.attr('width') / 2, svg.attr('height') / 2));
 
-const simulation = d3.forceSimulation(nodes)
-    .force('link', d3.forceLink(links).id(d => d.room))
-    .force('charge', d3.forceManyBody().strength(-200))
-    .force('center', d3.forceCenter(width / 2, height / 2));
+    const container = svg.append('g');
 
-const zoom = d3.zoom()
-    .scaleExtent([0.1, 10])
-    .on('zoom', zoomed);
+    const links = generateLinks(graphData);
+    const nodes = Array.from(new Set(graphData.map(d => d.room)))
+        .map(room => ({ room, color: getRandomColor() }));
 
-svg.call(zoom);
+    const link = container.selectAll('.link')
+        .data(links)
+        .enter().append('line')
+        .attr('class', 'link')
+        .style('stroke', 'black');
 
-const container = svg.append('g');
+    const node = container.selectAll('.node')
+        .data(nodes)
+        .enter().append('circle')
+        .attr('class', 'node')
+        .attr('r', 10)
+        .attr('fill', d => d.color)
+        .call(d3.drag()
+            .on('start', dragStart)
+            .on('drag', dragging)
+            .on('end', dragEnd));
 
-const link = container.selectAll('.link')
-    .data(links)
-    .enter().append('line')
-    .attr('class', 'link');
+    const labels = container.selectAll('.label')
+        .data(nodes)
+        .enter().append('text')
+        .attr('class', 'label')
+        .attr('dy', -15)
+        .attr('text-anchor', 'middle')
+        .text(d => d.room);
 
-const node = container.selectAll('.node')
-    .data(nodes)
-    .enter().append('circle')
-    .attr('class', 'node')
-    .attr('r', 10)
-    .attr('fill', d => d.color)
-    .call(d3.drag()
-        .on('start', dragStart)
-        .on('drag', dragging)
-        .on('end', dragEnd));
+    simulation.nodes(nodes);
+    simulation.force('link').links(links);
 
-const labels = container.selectAll('.label')
-    .data(nodes)
-    .enter().append('text')
-    .attr('class', 'label')
-    .attr('dy', -15)
-    .attr('text-anchor', 'middle')
-    .text(d => d.room);
+    simulation.on('tick', () => {
+        link
+            .attr('x1', d => d.source.x)
+            .attr('y1', d => d.source.y)
+            .attr('x2', d => d.target.x)
+            .attr('y2', d => d.target.y);
 
-simulation.on('tick', () => {
-    link
-        .attr('x1', d => d.source.x)
-        .attr('y1', d => d.source.y)
-        .attr('x2', d => d.target.x)
-        .attr('y2', d => d.target.y);
+        node
+            .attr('cx', d => d.x)
+            .attr('cy', d => d.y);
 
-    node
-        .attr('cx', d => d.x)
-        .attr('cy', d => d.y);
+        labels
+            .attr('x', d => d.x)
+            .attr('y', d => d.y);
+    });
 
-    labels
-        .attr('x', d => d.x)
-        .attr('y', d => d.y);
+    const zoom = d3.zoom()
+        .scaleExtent([0.1, 10])
+        .on('zoom', zoomed);
+
+    svg.call(zoom);
+
+    function zoomed(event) {
+        container.attr('transform', event.transform);
+    }
+
+    function dragStart(event, d) {
+        if (!event.active) simulation.alphaTarget(0.3).restart();
+        d.fx = d.x;
+        d.fy = d.y;
+    }
+
+    function dragging(event, d) {
+        d.fx = event.x;
+        d.fy = event.y;
+    }
+
+    function dragEnd(event, d) {
+        if (!event.active) simulation.alphaTarget(0);
+        d.fx = null;
+        d.fy = null;
+    }
+}
+const graphContainers = d3.selectAll('.graph-container');
+
+graphContainers.each(function(_, i) {
+    const graphContainer = d3.select(this);
+    const graphData = roomData; // You can implement your own data generation logic here
+    updateGraph(graphContainer, graphData);
 });
 
-function zoomed(event) {
-    container.attr('transform', event.transform);
-}
+const regenerateButton = d3.select('#regenerateButton');
 
-function dragStart(event, d) {
-    if (!event.active) simulation.alphaTarget(0.3).restart();
-    d.fx = d.x;
-    d.fy = d.y;
-}
+regenerateButton.on('click', function() {
+    graphContainers.selectAll('svg').remove(); // Clear existing graphs
 
-function dragging(event, d) {
-    d.fx = event.x;
-    d.fy = event.y;
-}
-
-function dragEnd(event, d) {
-    if (!event.active) simulation.alphaTarget(0);
-    d.fx = null;
-    d.fy = null;
-}
+    graphContainers.each(function(_, i) {
+        const graphContainer = d3.select(this);
+        const graphData = roomData; // You can implement your own data generation logic here
+        updateGraph(graphContainer, graphData);
+    });
+});
